7b6521814c588cebae8088366a4f770316cc164e
Syamsul <thesyamsulzaman@gmail.com>
2020-07-10 15:10:17 +0700

    Make The Navbar Responsive

--- app.js
+++ app.js
@@ -6,14 +6,21 @@ document.addEventListener("DOMContentLoaded", () => {
     scrollPos,
     docHeight;
 
+  let navLists = document.getElementById("nav__list");
+  let navToggleButton = document.getElementById("nav__toggle");
   let items = document.querySelectorAll("nav ul li");
-  items.forEach(item =>
-    item.addEventListener("click", function() {
+
+  navToggleButton.addEventListener("click", function () {
+    navLists.classList.toggle("nav__links--toggle");
+  });
+
+  items.forEach((item) =>
+    item.addEventListener("click", function () {
       //item.classList.remove("nav__item--active");
       //item.classList.add("nav__item--active");
     })
   );
-  window.addEventListener("scroll", function() {
+  window.addEventListener("scroll", function () {
     nav.className = docElem.scrollTop > 22 ? "nav--scroll" : "";
   });
 });
